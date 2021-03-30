c2909488e39ba770f04a46482f506343dbdc2abb
Khalem <33363455+Khalem@users.noreply.github.com>
2021-03-30 19:45:24 +0100

    moved input into it's own component, implemented label with animation, and changed SignInPage to class component

---
+++ src/components/custom-input/custom-input.component.jsx
@@ -0,0 +1,34 @@
+import React, { useEffect } from 'react';
+
+import './custom-input.styles.scss';
+
+const CustomInput = ({ handleChange, label, name, value, ...otherProps  }) => {
+    // if user has input anything into field, make label active
+    useEffect(() => {
+        const inputLabel = document.getElementById(label);
+
+        if (!value) {
+            inputLabel.classList.add('input-label');
+            inputLabel.classList.remove('active-label');
+
+        } else {
+            inputLabel.classList.add('active-label');
+            inputLabel.classList.remove('input-label');
+        }
+    }, [value]);
+
+    return (
+        <div className='input-container'>
+            <input 
+                className='input' 
+                name={name} 
+                onChange={handleChange}
+                value={value}
+                {...otherProps} 
+            />
+            <label for={name} className='input-label' id={label}>{label}</label>
+        </div>
+    );
+};
+
+export default CustomInput;
--- src/pages/sign-in/sign-in-page.component.jsx
+++ src/pages/sign-in/sign-in-page.component.jsx
@@ -1,18 +1,52 @@
 import React from 'react';
-import CustomButton from '../../components/custom-button/custom-button.component';
+
+import CustomInput from '../../components/custom-input/custom-input.component';
 
 import './sign-in-page.styles.scss';
 
-const SignInPage = () => (
-    <section className='sign-in'>
-        <div className='sign-in-box'>
-            <h1 className='sign-in-title'>Sign In</h1>
-            <input type='email' placeholder='email' className='input' />
-            <input type='password' placeholder='password' className='input' />
-            <button type='submit'>Sign In</button>
-            <p>Don't have an account?</p>
-        </div>
-    </section>
-);
+class SignInPage extends React.Component {
+    constructor() {
+        super();
+
+        this.state = {
+            email: '',
+            password: ''
+        }
+    }
+
+    handleChange = e => {
+        const { name, value } = e.target;
+
+        this.setState({ [name]: value });
+    }
+
+    render() {
+        return (
+            <section className='sign-in'>
+                <div className='sign-in-box'>
+                    <h1 className='sign-in-title'>Sign In</h1>
+                    <CustomInput 
+                        label='email'
+                        name='email'
+                        type='email'
+                        handleChange={this.handleChange}
+                        value={this.state.email}
+                        required
+                    />
+                    <CustomInput 
+                        label='password'
+                        name='password'
+                        type='password'
+                        handleChange={this.handleChange}
+                        value={this.state.password}
+                        required
+                    />
+                    <button type='submit'>Sign In</button>
+                    <p>Don't have an account?</p>
+                </div>
+            </section>
+        );
+    }
+};
 
 export default SignInPage;
