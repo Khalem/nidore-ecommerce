cb4d9501ff6a980aac2a664e9a3b9aabb186330a
Khalem <33363455+Khalem@users.noreply.github.com>
2021-04-07 00:12:29 +0100

    users now see different content when bag is empty

--- src/pages/bag/bag-page.component.jsx
+++ src/pages/bag/bag-page.component.jsx
@@ -1,12 +1,24 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { connect } from 'react-redux';
+import { withRouter } from 'react-router-dom';
 
 import CustomButton from '../../components/custom-button/custom-button.component';
 import BagItem from '../../components/bag-item/bag-item.component';
 
+import { ReactComponent as Pointer } from '../../assets/pointing-down.svg';
+
 import './bag-page.styles.scss';
 
-const BagPage = ({ bagItems }) => {
+const BagPage = ({ bagItems, history }) => {
+    const buttonStyles = {
+        backgroundColor: 'var(--dark-color)',
+        color: 'var(--background-color)',
+        boxShadow: 'none',
+        minWidth: '35%',
+        padding: '20px 10px',
+        fontWeight: '500'
+    };
+    
     const getCompleteTotal = () => {
         let completeTotal = 0;
         bagItems.forEach(({ price, quantity }) => {
@@ -16,27 +28,63 @@ const BagPage = ({ bagItems }) => {
         return completeTotal;
     };
 
-    const buttonStyles = {
-        backgroundColor: 'var(--dark-color)',
-        color: 'var(--background-color)',
-        boxShadow: 'none',
-        minWidth: '35%',
-        padding: '20px 10px',
-        fontWeight: '500'
-    };
+    // move pointer to cursor position on mouse enter
+    const mouseEnterRedirect = e => {
+        const pointer = document.getElementsByClassName('pointer')[0];
+        const pointerPos = pointer.getBoundingClientRect();
+        const titlePos = e.target.getBoundingClientRect();
+
+        const x = titlePos.left - pointerPos.left;
+
+        pointer.style.transform = `translateX(${x + 25}px)`;
+    }
+
+    // reset transform on mouse leave
+    const mouseLeaveRedirect = () => {
+        const pointer = document.getElementsByClassName('pointer')[0];
+
+        pointer.style.transform = 'translateX(0)';
+    }
 
     return (
         <div className='bag-page'>
-            <h1 className='bag-page-title'>Your shopping bag</h1>
             {
-                bagItems.map(item => 
-                    <BagItem item={item} />
-                )
+                bagItems.length ?
+                <Fragment>
+                    <h1 className='bag-page-title'>Your shopping bag</h1>
+                    {
+                        bagItems.map(item => 
+                            <BagItem item={item} />
+                        )
+                    }
+                    <div className='bag-cta'>
+                        <h1 className='complete-total'>total: €{getCompleteTotal()}</h1>
+                        <CustomButton name='checkout' style={buttonStyles}>Go to Checkout</CustomButton>
+                    </div>
+                </Fragment>
+                :
+                <Fragment>
+                    <h1 className='bag-page-title'>Nothing here yet,</h1>
+                    <h2 className='bag-page-secondary-title'>Let's change that</h2>
+                    <Pointer className='pointer' />
+                    <div className='redirect-container'>
+                        <h3 className='redirect-title' 
+                            onMouseEnter={mouseEnterRedirect} 
+                            onMouseLeave={mouseLeaveRedirect}
+                            onClick={() => history.push(`/womens`)}
+                        >
+                            Womens
+                        </h3>
+                        <h3 className='redirect-title' 
+                            onMouseEnter={mouseEnterRedirect} 
+                            onMouseLeave={mouseLeaveRedirect}
+                            onClick={() => history.push(`/mens`)}
+                        >
+                            Mens
+                        </h3>
+                    </div>
+                </Fragment>
             }
-            <div className='bag-cta'>
-                <h1 className='complete-total'>total: €{getCompleteTotal()}</h1>
-                <CustomButton name='checkout' style={buttonStyles}>Go to Checkout</CustomButton>
-            </div>
         </div>
     );
 };
@@ -45,4 +93,4 @@ const mapStateToProps = ({ bag }) => ({
     bagItems: bag.bagItems
 });
 
-export default connect(mapStateToProps)(BagPage);
+export default connect(mapStateToProps)(withRouter(BagPage));
